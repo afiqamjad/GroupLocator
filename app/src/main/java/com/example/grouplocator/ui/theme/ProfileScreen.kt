06fa5fcf845ef3e6a0745a62a80efec5327cc605
Afiq Amjad bin Khairir <afiqk@bu.edu>
2024-05-20 10:51:03 -0400

    Added clickable profile picture card on Profile Screen

--- app/src/main/java/com/example/grouplocator/ui/theme/ProfileScreen.kt
+++ app/src/main/java/com/example/grouplocator/ui/theme/ProfileScreen.kt
@@ -1,19 +1,25 @@
 package com.example.grouplocator.ui.theme
 
-import android.graphics.drawable.shapes.Shape
-import android.widget.ImageButton
+import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.grouplocator.R
 
 @Composable
 fun ProfileScreen() {
@@ -28,8 +34,22 @@ fun ProfileScreen() {
 
 @Composable
 fun ProfilePicture() {
-    Card(shape = RoundedCornerShape(100), modifier = Modifier.padding(top = 50.dp, bottom = 10.dp)) {
-        Text(text = "Hello", modifier = Modifier.padding(10.dp))
+    val context = LocalContext.current
+    Card(shape = CircleShape, modifier = Modifier
+        .padding(top = 90.dp, bottom = 10.dp)
+        .clip(shape = CircleShape)
+        .clickable {
+            Toast
+                .makeText(context, "Clicked!", Toast.LENGTH_SHORT)
+                .show()
+        }) {
+        Image(painter = painterResource(id = R.drawable.pfp_default),
+            contentDescription = "default profile picture",
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .size(90.dp)
+                .padding(7.dp),
+            colorFilter = ColorFilter.tint(color = Color.LightGray))
     }
 }
 
