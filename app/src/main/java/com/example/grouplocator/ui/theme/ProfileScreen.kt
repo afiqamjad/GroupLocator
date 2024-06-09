203beab55e785d6647492e24a8b0495b26c603c4
Afiq Amjad bin Khairir <afiqk@bu.edu>
2024-06-09 16:45:37 -0400

    Added Past Events List

--- app/src/main/java/com/example/grouplocator/ui/theme/ProfileScreen.kt
+++ app/src/main/java/com/example/grouplocator/ui/theme/ProfileScreen.kt
@@ -9,11 +9,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,18 +37,41 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.example.grouplocator.R
+import java.util.Date
+
+data class Event(val id: Int, val date: Date, val title: String)
 
 @Composable
 fun ProfileScreen() {
+    val events = listOf(
+        Event(1, date = Date(), "Ethan's Party"),
+        Event(2, date = Date(), "Nomikai"),
+        Event(3, date = Date(), "Frat"),
+        Event(4, date = Date(), "Nomikai 5"),
+        Event(5, date = Date(), "Party @ Elena's"),
+        Event(6, date = Date(), "Rando"),
+        Event(7, date = Date(), "Cambridge Nomikai")
+    )
     Column(modifier = Modifier
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
         ) {
         ProfilePicture()
         Username(username = "afiqkhairir")
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "Past Events",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(start = 15.dp, end = 0.dp, top = 25.dp, bottom = 4.dp),
+                fontWeight = FontWeight.SemiBold
+            )
+            PastEventsList(events = events)
+        }
     }
 }
 
@@ -77,6 +109,41 @@ fun ProfilePicture() {
                     modifier = Modifier.fillMaxSize(),
                     colorFilter = ColorFilter.tint(color = Color.LightGray)
                 )
+            }
+        }
+    }
+}
+
+@Composable
+fun PastEventsCard(event: Event) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+
+    ){
+        Column {
+            Text(text = event.title, fontSize = 20.sp, color = Color.Black)
+            Spacer(modifier = Modifier.height(3.dp))
+            Text(text = event.date.toString(), fontSize = 14.sp, color = Color.Gray)
+        }
+    }
+}
+
+@Composable
+fun PastEventsList(events: List<Event>) {
+    Card (
+        shape = RoundedCornerShape(10.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .heightIn(min = 0.dp, max = 215.dp)
+    ){
+        LazyColumn(modifier = Modifier.padding(5.dp)) {
+            items(events) {event ->
+                PastEventsCard(event = event)
             }
         }
     }
