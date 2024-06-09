24e80c2ba1fb91a03b4bf80e1e610ffd71809b63
Afiq Amjad bin Khairir <afiqk@bu.edu>
2024-06-09 14:44:10 -0400

    Added functionality of uploading profile picture

--- app/src/main/java/com/example/grouplocator/ui/theme/ProfileScreen.kt
+++ app/src/main/java/com/example/grouplocator/ui/theme/ProfileScreen.kt
@@ -1,8 +1,13 @@
 package com.example.grouplocator.ui.theme
 
+import android.net.Uri
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,14 +16,20 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import com.example.grouplocator.R
 
 @Composable
@@ -35,21 +46,39 @@ fun ProfileScreen() {
 @Composable
 fun ProfilePicture() {
     val context = LocalContext.current
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri: Uri? ->
+        imageUri = uri
+    }
     Card(shape = CircleShape, modifier = Modifier
         .padding(top = 90.dp, bottom = 10.dp)
         .clip(shape = CircleShape)
         .clickable {
-            Toast
-                .makeText(context, "Clicked!", Toast.LENGTH_SHORT)
-                .show()
+            launcher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }) {
-        Image(painter = painterResource(id = R.drawable.pfp_default),
-            contentDescription = "default profile picture",
-            alignment = Alignment.Center,
+        Box(
             modifier = Modifier
                 .size(90.dp)
-                .padding(7.dp),
-            colorFilter = ColorFilter.tint(color = Color.LightGray))
+                .clip(CircleShape)
+        ) {
+            if (imageUri != null) {
+                Image(
+                    painter = rememberAsyncImagePainter(imageUri),
+                    contentDescription = "selected profile picture",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.pfp_default),
+                    contentDescription = "default profile picture",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize(),
+                    colorFilter = ColorFilter.tint(color = Color.LightGray)
+                )
+            }
+        }
     }
 }
 
